1b027f1fbe40555c6cefaa21884e9aa884241c77
Beni <beniamin.silas@gmail.com>
2020-01-11 14:40:40 +0200

    Added libraries to the project structure

---
+++ src/main/java/ITeaHome.java
@@ -0,0 +1,10 @@
+
+import org.joda.time.LocalDate;
+
+
+public class ITeaHome {
+    public static void main(String[] args) {
+        LocalDate localDate = new LocalDate();
+        System.out.println("Local date is : " + localDate);
+    }
+}
--- untitled/src/iteahome.java
+++
@@ -1,5 +0,0 @@
-public class iteahome {
-    public static void main(String[] args) {
-        System.out.println("Hello ITea Home !!");
-    }
-}
